cc4de7e1645b0b8fb51486caff607ccbbd0eaa55
Rene Leveille <reneb.leveille@gmail.com>
2017-05-30 20:42:14 -0400

    Create header, footer templates with index.php and style.css

---
+++ index.php
@@ -0,0 +1,8 @@
+<?php get_header(); ?>
+
+<body>
+    <h1>Hello, world!</h1>
+    <?php echo bloginfo('stylesheet_directory');?>/style.css <br>
+    <?php echo get_stylesheet_uri(); ?>/style.css
+
+<?php get_footer(); ?>
